355030e512162456b8f937a21502bc0f1597b32b
suriya <msuriyak.2495@gmail.com>
2017-04-18 07:09:25 +0530

    Minor modification

--- shallow_water.py
+++ shallow_water.py
@@ -98,33 +98,3 @@ class shallow_water(object):
 if __name__ == '__main__':
     print('Running main!!!')
     a = shallow_water(100, 100, N_x=64, N_y=64)#, x_start=0, x_end=5, y_start=0, y_end=5)
-    import sys
-    from numbers import Number
-    from collections import Set, Mapping, deque
-
-    zero_depth_bases = (str, bytes, Number, range, bytearray)
-    iteritems = 'items'
-
-    def getsize(obj_0):
-        """Recursively iterate to sum size of object & members."""
-        def inner(obj, _seen_ids = set()):
-            obj_id = id(obj)
-            if obj_id in _seen_ids:
-                return 0
-            _seen_ids.add(obj_id)
-            size = sys.getsizeof(obj)
-            if isinstance(obj, zero_depth_bases):
-                pass # bypass remaining control flow and return
-            elif isinstance(obj, (tuple, list, Set, deque)):
-                size += sum(inner(i) for i in obj)
-            elif isinstance(obj, Mapping) or hasattr(obj, iteritems):
-                size += sum(inner(k) + inner(v) for k, v in getattr(obj, iteritems)())
-            # Check for custom object instances - may subclass above too
-            if hasattr(obj, '__dict__'):
-                size += inner(vars(obj))
-            if hasattr(obj, '__slots__'): # can have __slots__ with __dict__
-                size += sum(inner(getattr(obj, s)) for s in obj.__slots__ if hasattr(obj, s))
-            return size
-        return inner(obj_0)
-
-    print('Size of class : %2.3f MB' %(getsize(a)/(8*1024*1024)))
