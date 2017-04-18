0693f9f89d9b7cf20e8f7a2d4d52846881d9fa90
suriya <msuriyak.2495@gmail.com>
2017-04-18 07:08:47 +0530

    Mass, derivative and flux matrices created and shallow water class init created

--- matrices.py
+++ matrices.py
@@ -4,7 +4,10 @@ from interpolation import *
 from integration import *
 from nodes_and_weights import *
 
-def mass_matrix(N, quad_nodes='lobatto', quad_order=32):
+##### kth 2d interpolation is pdt of k = j*(N_x + 1) + i
+##### xi_k = (phi_i1*psi_i2) * (phi_j1*psi_j2)
+
+def mass_matrix(N, *, quad_nodes='lobatto', quad_order=64):
     points = get_lobatto_points(N + 1)
     
     if quad_nodes =='legendre':
@@ -28,6 +31,122 @@ def mass_matrix(N, quad_nodes='lobatto', quad_order=32):
                 result[i, j] = integration(temp, -1, 1, nodes, weights)
     return result
 
+def mass_matrix_2d(N_x, N_y, *, quad_nodes_x='lobatto', quad_nodes_y='lobatto', quad_order_x=64, quad_order_y=64):
+    mass_x = mass_matrix(N_x, quad_nodes=quad_nodes_x, quad_order=quad_order_x)
+    mass_y = mass_matrix(N_y, quad_nodes=quad_nodes_y, quad_order=quad_order_y)
+
+    result = np.zeros(((N_x + 1)*(N_y + 1), (N_x + 1)*(N_y + 1)))
+
+    for i1 in range(N_x + 1):
+        for j1 in range(N_x + 1):
+            for i2 in range(N_y + 1):
+                for j2 in range(N_y + 1):
+                    result[i2*N_x + i2 + i1, j2*N_x + j2 + j1] = mass_x[i1, j1]*mass_y[i2, j2]
+
+    return result
+
+def derivative_matrix(N, *, quad_nodes='lobatto', quad_order=64):
+    points = get_lobatto_points(N + 1)
+    
+    if quad_nodes =='legendre':
+        nodes = get_legendre_points(quad_order + 1)
+        weights = get_legendre_weights(quad_order + 1)
+
+    else :
+        nodes = get_lobatto_points(quad_order + 1)
+        weights = get_lobatto_weights(quad_order + 1)
+
+    lag_interpolation = lagrange_1d(points)
+
+    result = np.zeros((N + 1, N + 1))
+    for i in range(N+1) :
+        for j in range(N+1) :
+            def temp(x):
+                return lag_interpolation.deriv_basis[i](x)*lag_interpolation.basis_functions[j](x)
+            result[i, j] = integration(temp, -1, 1, nodes, weights)
+    return result
+
+def derivative_matrix_2d(N_x, N_y, *, quad_nodes_x='lobatto', quad_nodes_y='lobatto', quad_order_x=64, quad_order_y=64):
+    derivative_x = derivative_matrix(N_x, quad_nodes=quad_nodes_x, quad_order=quad_order_x)
+    derivative_y = derivative_matrix(N_y, quad_nodes=quad_nodes_y, quad_order=quad_order_y)
+
+    mass_x = mass_matrix(N_x, quad_nodes=quad_nodes_x, quad_order=quad_order_x)
+    mass_y = mass_matrix(N_y, quad_nodes=quad_nodes_y, quad_order=quad_order_y)
+
+    deriv_x = np.zeros(((N_x + 1)*(N_y + 1), (N_x + 1)*(N_y + 1)))
+    deriv_y = np.zeros(((N_x + 1)*(N_y + 1), (N_x + 1)*(N_y + 1)))
+
+    for i1 in range(N_x + 1):
+        for j1 in range(N_x + 1):
+            for i2 in range(N_y + 1):
+                for j2 in range(N_y + 1):
+                    deriv_x[i2*N_x + i2 + i1, j2*N_x + j2 + j1] = mass_y[i2, j2]*derivative_x[i1, j1]
+
+    for i1 in range(N_x + 1):
+        for j1 in range(N_x + 1):
+            for i2 in range(N_y + 1):
+                for j2 in range(N_y + 1):
+                    deriv_y[i2*N_x + i2 + i1, j2*N_x + j2 + j1] = mass_x[i1, j1]*derivative_y[i2, j2]
+
+    return deriv_x, deriv_y
+
+def flux_matrix(N):
+    points = get_lobatto_points(N + 1)
+
+    lag_interpolation = lagrange_1d(points)
+
+    result = np.zeros((N + 1, N + 1))
+    for i in range(N+1) :
+        for j in range(N+1) :
+            def temp(x):
+                return lag_interpolation.basis_functions[i](x)*lag_interpolation.basis_functions[j](x)
+            result[i, j] = temp(1) - temp(-1)
+    return result 
+
+def flux_matrix_2d(N_x, N_y, *, quad_nodes_x='lobatto', quad_nodes_y='lobatto', quad_order_x=64, quad_order_y=64):
+    up    = np.zeros(((N_x + 1)*(N_y + 1), (N_x + 1)*(N_y + 1)))
+    down  = np.zeros(((N_x + 1)*(N_y + 1), (N_x + 1)*(N_y + 1)))
+    right = np.zeros(((N_x + 1)*(N_y + 1), (N_x + 1)*(N_y + 1)))
+    left  = np.zeros(((N_x + 1)*(N_y + 1), (N_x + 1)*(N_y + 1)))
+
+    mass_x = mass_matrix(N_x, quad_nodes=quad_nodes_x, quad_order=quad_order_x)
+    mass_y = mass_matrix(N_y, quad_nodes=quad_nodes_y, quad_order=quad_order_y)
+
+    i2=j2=0
+    for i1 in range(N_x + 1) :
+        for j1 in range(N_x + 1) :
+            down[i2*N_x + i2 + i1, j2*N_x + j2 + j1] = mass_x[i1, j1]
+
+    i2=j2=N_y
+    for i1 in range(N_x + 1) :
+        for j1 in range(N_x + 1) :
+            up[i2*N_x + i2 + i1, j2*N_x + j2 + j1] = mass_x[i1, j1]
+
+    i1=j1=0
+    for i2 in range(N_y + 1) :
+        for j2 in range(N_y + 1) :
+            left[i2*N_x + i2 + i1, j2*N_x + j2 + j1] = mass_x[i2, j2]
+
+    i1=j1=N_x
+    for i2 in range(N_y + 1) :
+        for j2 in range(N_y + 1) :
+            right[i2*N_x + i2 + i1, j2*N_x + j2 + j1] = mass_x[i2, j2]
+    
+    return down, right, up, left
+
+
 if __name__ == '__main__':
     print('Running main!!!')
-    print(mass_matrix(1, quad_order=2))
+    print(3*mass_matrix(1, quad_order=2))
+    print(9*mass_matrix_2d(1, 1, quad_order_x=2, quad_order_y=2))
+
+    print(2*derivative_matrix(1, quad_order=2))
+    temp = derivative_matrix_2d(1, 1, quad_order_x=2, quad_order_y=2)
+    print(6*temp[0])
+    print(6*temp[1])
+
+    print(flux_matrix(1))
+    print(3*flux_matrix_2d(1, 1)[0])
+    print(3*flux_matrix_2d(1, 1)[1])
+    print(3*flux_matrix_2d(1, 1)[2])
+    print(3*flux_matrix_2d(1, 1)[3])
---
+++ shallow_water.py
@@ -0,0 +1,130 @@
+from __future__ import division
+import numpy as np
+import os
+from tqdm import tqdm
+
+from interpolation import *
+from integration import *
+from nodes_and_weights import *
+from matrices import *
+
+class shallow_water(object):
+    # order of the polynomial interpolation (used in p-refinement)
+    N_x = None # order of interpolation in x-direction
+    N_y = None # order of interpolation in y-direction
+    nodes_x = None
+    nodes_y = None
+
+    # Elements in grid (used in h-refinement)
+    Nex = None # number of elements in x-direction
+    Ney = None # number of elements in y-direction
+    
+    # Grid co-ordinates
+    X   = None # x-co-ordinate
+    Y   = None # y-co-ordinate
+
+    # Domain details
+    x_start = None # X-coordinate starting
+    x_end   = None # X-coordinate ending
+    y_start = None # Y-coordinate starting
+    y_end   = None # Y-coordinate ending
+    delta_x = None # x element size
+    delta_y = None # y-element size
+
+    # Water height, velocity and mean height
+    eta = None # total height of water
+    H   = None # total height to mean level
+    u   = None # x-velocity
+    v   = None # y-velocity
+
+    H_x = None
+    H_y = None
+    h_x = None
+    h_y = None
+
+    # Simulation specs
+    dt = None
+    time_steps = None
+    end_time = None
+
+    def __init__(self, Nex, Ney, *, N=None, N_x=8, N_y=8, x_start=-5, x_end=5, y_start=-5, y_end=5):
+        if N != None:
+            self.N_x = N
+            self.N_y = N
+        else :
+            self.N_x = N_x
+            self.N_y = N_y
+
+        self.Nex = Nex
+        self.Ney = Ney
+
+        self.x_start = x_start
+        self.x_end   = x_end
+        self.y_start = y_start
+        self.y_end   = y_end
+
+        self.delta_x = (x_end - x_start)/Nex
+        self.delta_y = (y_end - y_start)/Ney
+
+        self.nodes_x = get_lobatto_points(self.N_x + 1)*self.delta_x/2
+        self.nodes_y = get_lobatto_points(self.N_y + 1)*self.delta_y/2
+
+        x_mid = np.linspace(x_start, x_end, Nex + 1)[:-1] + self.delta_x/2
+        y_mid = np.linspace(y_start, y_end, Ney + 1)[:-1] + self.delta_y/2
+
+        x_temp = x_mid[0] + self.nodes_x
+        for i in range(1, Nex):
+            x_temp = np.concatenate((x_temp, self.nodes_x + x_mid[i]))
+
+        y_temp = y_mid[0] + self.nodes_y
+        for i in range(1, Ney):
+            y_temp = np.concatenate((y_temp, self.nodes_y + y_mid[i]))
+    
+        self.X = np.array(x_temp.tolist()*Ney*(self.N_y + 1)).reshape((Ney*(self.N_y + 1), Nex*(self.N_x + 1)))
+        self.Y = np.array(y_temp.tolist()*Nex*(self.N_x + 1)).reshape((Ney*(self.N_y + 1), Nex*(self.N_x + 1))).T
+
+        del x_mid, x_temp, y_mid, y_temp, i
+
+        self.eta = np.zeros(self.X.shape)
+        self.H = np.zeros(self.X.shape)
+        self.u = np.zeros(self.X.shape)
+        self.v = np.zeros(self.X.shape)
+        self.H_x = np.zeros(self.X.shape)
+        self.H_y = np.zeros(self.X.shape)
+        self.h_x = np.zeros(self.X.shape)
+        self.h_y = np.zeros(self.X.shape)
+
+
+if __name__ == '__main__':
+    print('Running main!!!')
+    a = shallow_water(100, 100, N_x=64, N_y=64)#, x_start=0, x_end=5, y_start=0, y_end=5)
+    import sys
+    from numbers import Number
+    from collections import Set, Mapping, deque
+
+    zero_depth_bases = (str, bytes, Number, range, bytearray)
+    iteritems = 'items'
+
+    def getsize(obj_0):
+        """Recursively iterate to sum size of object & members."""
+        def inner(obj, _seen_ids = set()):
+            obj_id = id(obj)
+            if obj_id in _seen_ids:
+                return 0
+            _seen_ids.add(obj_id)
+            size = sys.getsizeof(obj)
+            if isinstance(obj, zero_depth_bases):
+                pass # bypass remaining control flow and return
+            elif isinstance(obj, (tuple, list, Set, deque)):
+                size += sum(inner(i) for i in obj)
+            elif isinstance(obj, Mapping) or hasattr(obj, iteritems):
+                size += sum(inner(k) + inner(v) for k, v in getattr(obj, iteritems)())
+            # Check for custom object instances - may subclass above too
+            if hasattr(obj, '__dict__'):
+                size += inner(vars(obj))
+            if hasattr(obj, '__slots__'): # can have __slots__ with __dict__
+                size += sum(inner(getattr(obj, s)) for s in obj.__slots__ if hasattr(obj, s))
+            return size
+        return inner(obj_0)
+
+    print('Size of class : %2.3f MB' %(getsize(a)/(8*1024*1024)))
